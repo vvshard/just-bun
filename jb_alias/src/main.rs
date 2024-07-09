2fa738ad9bb3e7f46f958a8abb70a4ad5cc9ceb5
Vladimir Shardinov <vvshard@gmail.com>
2024-07-09 14:09:31 +0300

    got rid of the dependence on the exe file name

--- jb_alias/src/main.rs
+++ jb_alias/src/main.rs
@@ -9,16 +9,17 @@ fn main() {
     // };
 
     let mut path = env::current_exe().expect("Failed to get path to exe");
+    let file_stem = path
+        .file_stem()
+        .expect("Failed to get exe file name.")
+        .to_str()
+        .expect("The exe file name is not Unicode")
+        .to_owned();
     path.pop();
     path.pop();
-    path.push("jb_global/main.js");
+    path.push(&format!("{file_stem}_global/main.js"));
 
     let args = env::args();
-    // dbg!(&args);
 
-    Command::new("bun")
-        .arg(path)
-        .args(args.skip(1))
-        .status()
-        .expect("Failed to start bun");
+    Command::new("bun").arg(path).args(args.skip(1)).status().expect("Failed to start bun");
 }
