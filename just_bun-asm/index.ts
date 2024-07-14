b827a53ad7548b65a4426a2eaf5df8eafc65a4cd
Vladimir Shardinov <vvshard@gmail.com>
2024-07-14 15:30:41 +0300

    parseRecipes()

--- just_bun-asm/index.ts
+++ just_bun-asm/index.ts
@@ -46,7 +46,7 @@ export async function start(args: string[]) {
         return err(`${runnerPath}/+justbun.mjs does not contain function runRecipe()`);
 
     if (displayList)
-        return csl(parseRecipes(runRecipe.toString()));
+        return csl(`List of recipes in ${runnerPath}/+justbun.mjs: \n${parseRecipes(runRecipe.toString())}`);
 
     if (isGlob) {
         args.shift();
@@ -116,9 +116,52 @@ bun build ./main.ts --outdir ../ --target bun
 `.cwd(jb_global + "/mainupdate");
 }
 
+/** Returns a list of recipes based on the text of the function */
 function parseRecipes(sfun: string): string {
+    let re = /\b(?:switch\s*\([^\)]+\)|case\s*(?:"[^"]*|void 0)"?\s*:|return\s*|break\s*;|default\s*:)/g;
+    let list = "";
+    let alias = 0;
 
-    return sfun;
+    const fStart = (a: string[]) => {
+        if (a[0] === 'switch')
+            stateF = fMain;
+    };
+    const fMain = (a: string[]) => {
+        switch (a[0]) {
+            case 'case':
+                list += ['', ' / '][alias] ?? ' ';
+                alias += 1;
+                list += a[1] === 'void' && a[2] === '0' ? '<default>' : a[1];
+                break;
+            case 'break':
+            case 'return':
+                list += '\n';
+                alias = 0;
+                break;
+            case 'default':
+                stateF = null;
+                break;
+            case 'switch':
+                stateF = fSkip;
+                break;
+            default:
+                break;
+        }
+    };
+    const fSkip = (a: string[]) => {
+        if (a[0] === 'default')
+            stateF = fMain;
+    };
+
+    let stateF: ((a: string[]) => void) | null = fStart;
+    let arr: RegExpExecArray | null;
+    while ((arr = re.exec(sfun)) !== null && stateF !== null) {
+        // console.log(`arr[0]= ${arr[0]}`);
+        let a = arr[0].split(/ ["\(]|[\);: "]/);
+        stateF(a);
+    }
+
+    return list;
 }
 
 async function jb_from_template(tmplName = '_') {
